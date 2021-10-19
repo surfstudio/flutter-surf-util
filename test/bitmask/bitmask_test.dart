870636ac7dce8fead4d5789f2c8484ff23f1faaf
Tatiana Suginagit config --global user.name Tatiana <internetova@yandex.ru>
2021-10-19 17:21:30 +0300

    fix test bitmask

--- test/bitmask/bitmask_test.dart
+++ test/bitmask/bitmask_test.dart
@@ -19,23 +19,182 @@ class TestBitmask extends Bitmask {
   const TestBitmask(int value) : super(value);
 }
 
-void main() {
-  group('Bitmask', () {
-    test('getMask returns aggregated mask for provided values', () {
-      expect(
-        Bitmask.getMask(const [
-          TestBitmask(0x0001),
-          TestBitmask(0x0002),
-          TestBitmask(0x0008),
-          TestBitmask(0x0200),
-        ]),
-        equals(0x020b),
-      );
-    });
+/// binary representation
+/// 0000 0101 -> 5
 
-    test('isOn returns true if checked value contains provided mask', () {
-      expect(const TestBitmask(0x0001).isOn(0xFFFFFF), isTrue);
-      expect(const TestBitmask(0x1000).isOn(0xFFEFFF), isFalse);
-    });
+Map<int, List<TestBitmask>> testedData = {
+  0: [],
+  1: const [TestBitmask(1)],
+  2: const [TestBitmask(2)],
+  3: const [TestBitmask(1), TestBitmask(2)],
+  4: const [TestBitmask(4)],
+  5: const [TestBitmask(1), TestBitmask(4)],
+  6: const [TestBitmask(2), TestBitmask(4)],
+  7: const [TestBitmask(1), TestBitmask(2), TestBitmask(4)],
+  8: const [TestBitmask(8)],
+  9: const [TestBitmask(1), TestBitmask(8)],
+  10: const [TestBitmask(2), TestBitmask(8)],
+  11: const [TestBitmask(1), TestBitmask(2), TestBitmask(8)],
+  12: const [TestBitmask(4), TestBitmask(8)],
+  13: const [TestBitmask(1), TestBitmask(4), TestBitmask(8)],
+  14: const [TestBitmask(2), TestBitmask(4), TestBitmask(8)],
+  15: const [TestBitmask(1), TestBitmask(2), TestBitmask(4), TestBitmask(8)],
+};
+
+void main() {
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(0).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(0).value),
+        equals(testedData.entries.elementAt(0).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(1).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(1).value),
+        equals(testedData.entries.elementAt(1).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(2).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(2).value),
+        equals(testedData.entries.elementAt(2).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(3).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(3).value),
+        equals(testedData.entries.elementAt(3).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(4).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(4).value),
+        equals(testedData.entries.elementAt(4).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(5).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(5).value),
+        equals(testedData.entries.elementAt(5).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(6).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(6).value),
+        equals(testedData.entries.elementAt(6).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(7).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(7).value),
+        equals(testedData.entries.elementAt(7).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(8).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(8).value),
+        equals(testedData.entries.elementAt(8).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(9).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(9).value),
+        equals(testedData.entries.elementAt(9).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(10).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(10).value),
+        equals(testedData.entries.elementAt(10).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(11).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(11).value),
+        equals(testedData.entries.elementAt(11).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(12).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(12).value),
+        equals(testedData.entries.elementAt(12).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(13).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(13).value),
+        equals(testedData.entries.elementAt(13).key),
+      );
+    },
+  );
+
+  test(
+    'getMask returns aggregated mask ${testedData.entries.elementAt(14).key} for provided values',
+    () {
+      expect(
+        Bitmask.getMask(testedData.entries.elementAt(14).value),
+        equals(testedData.entries.elementAt(14).key),
+      );
+    },
+  );
+
+  test('isOn returns true if checked value contains provided mask', () {
+    expect(const TestBitmask(1).isOn(1), isTrue);
+    expect(const TestBitmask(8).isOn(15), isTrue);
+    expect(const TestBitmask(2).isOn(8), isFalse);
   });
 }
